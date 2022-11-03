aaa26b427fa9cf79fb3cc9e612ba5b54f03aa50f
JMariadlcs <jmariadelacruzsanchez@gmail.com>
2022-11-03 19:23:45 +0100

    number comparison using cmp

--- chapter_2/projects/guessing_game/src/main.rs
+++ chapter_2/projects/guessing_game/src/main.rs
@@ -1,5 +1,6 @@
 use std::io; 
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Welcome to The Guessing Game");
@@ -19,7 +20,18 @@ fn main() {
         .read_line(&mut guess) // variable used here must be "mut"
                                // '&' is a "reference" access to memory to data directly (like a pointer in C)
         .expect("Failed to read line");
+    
+    // SHADOWING -> redefine 'guess' another time to change its type (u32)
+    // trim() eliminates blank spaces at the starting and end part of a String
+    // parse() converts String to specified type (u32)
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
     println!("Your guess was: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Guess is too small."),
+        Ordering::Greater => println!("Guess is to high"),
+        Ordering::Equal => println!("Correct guess!!"),
+    }
 
 }
