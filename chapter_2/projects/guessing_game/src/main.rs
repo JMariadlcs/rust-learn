b24a32480cced38ef3df255c948b36217adaefce
JMariadlcs <jmariadelacruzsanchez@gmail.com>
2022-11-03 18:54:03 +0100

    random number generaed

--- chapter_2/projects/guessing_game/src/main.rs
+++ chapter_2/projects/guessing_game/src/main.rs
@@ -1,8 +1,15 @@
 use std::io; 
+use rand::Rng;
 
 fn main() {
     println!("Welcome to The Guessing Game");
 
+    // Generate Secret Number
+    // rand::tread_rng indicates that we are generating a random num in the actual thread
+    // gen_range indicates range of the random number
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The generated secret number is: {}", secret_number);
     println!("Please give an input");
 
     let mut guess = String::new(); // mut -> mutable variable 
