faf4667b745a874584cb400a574ed4191dd33ff5
JMariadlcs <jmariadelacruzsanchez@gmail.com>
2022-11-03 18:34:27 +0100

    commens on guessing game

--- chapter_2/projects/guessing_game/src/main.rs
+++ chapter_2/projects/guessing_game/src/main.rs
@@ -5,10 +5,12 @@ fn main() {
 
     println!("Please give an input");
 
-    let mut guess = String::new();
+    let mut guess = String::new(); // mut -> mutable variable 
+                                   // variables are immutable by default
 
     io::stdin()
-        .read_line(&mut guess)  
+        .read_line(&mut guess) // variable used here must be "mut"
+                               // '&' is a "reference" access to memory to data directly (like a pointer in C)
         .expect("Failed to read line");
 
     println!("Your guess was: {}", guess);
