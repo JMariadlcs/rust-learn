a172038398d6bedf44ae2aa34364456330c75b9a
JMariadlcs <jmariadelacruzsanchez@gmail.com>
2022-11-04 00:26:19 +0100

    shadowing

--- chapter_3/projects/variables/src/main.rs
+++ chapter_3/projects/variables/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    // let x = 5; // by default variables are immutable
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
+
+    // SHADOWING
+
+    let y = 5;
+
+    let y = y + 1; // redefine same immutable variable shadows previous one
+
+    {
+        let y = y * 2;
+        println!("The value of x in the inner scope is: {y}");
+    }
+
+    println!("The value of x is: {y}");
 }
