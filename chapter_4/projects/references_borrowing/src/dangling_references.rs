90c5b3d899e28a6aae063df3a1537593f49d8038
JMariadlcs <jmariadelacruzsanchez@gmail.com>
2022-11-05 17:08:54 +0100

    slice_type

--- chapter_4/projects/references_borrowing/src/dangling_references.rs
+++ chapter_4/projects/references_borrowing/src/dangling_references.rs
@@ -1,5 +1,6 @@
 fn main() {
     let reference_to_nothing = dangle();
+    let reference_to_something = no_dangle();
 }
 
 fn dangle() -> &String {
@@ -7,4 +8,10 @@ fn dangle() -> &String {
 
     &s // As s is going out of scope after the function finished
        // It can not be returned a reference to it -> ITS OUT OF SCOPE!
+}
+
+fn no_dangle() -> String {
+    let s = String::from("hello");
+
+    s // We are returning s not a reference to s. So it works!
 }
