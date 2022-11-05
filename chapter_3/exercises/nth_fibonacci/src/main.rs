fac631587e6f0cd34c8fa65c091e9511f278f07f
JMariadlcs <jmariadelacruzsanchez@gmail.com>
2022-11-05 18:20:12 +0100

    fibo exercise

--- chapter_3/exercises/nth_fibonacci/src/main.rs
+++ chapter_3/exercises/nth_fibonacci/src/main.rs
@@ -1,3 +1,29 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, nth fibonacci!");
+
+    println!("Introduce a fibonacci number.");
+
+    let mut n = String::new();
+    
+    io::stdin().read_line(&mut n).expect("Failed to read line");
+
+    let n: u32 = match n.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {panic!("Please input a valid positive number");}
+    };
+
+    let fibo_num = nth_fibonacci(n);
+    print!("The fibo number of {} is {}", n, fibo_num)
+}
+
+fn nth_fibonacci( n:u32 ) -> u32 {
+    // Fibonacci of n is (n-2 + n-1)
+    if n == 1 {
+        return 1;
+    } else if n == 0 {
+        return 0;
+    }
+    (nth_fibonacci(n - 1)) + nth_fibonacci(n - 2)
 }
