5af758a9d5841ed040a7801cef32da6d2c8837b5
JMariadlcs <jmariadelacruzsanchez@gmail.com>
2022-11-05 13:33:51 +0100

    allocating and freeing memory

--- chapter_4/projects/ownership/src/main.rs
+++ chapter_4/projects/ownership/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, Ownership!");
+    let mut s = String::from("hello"); // 'String' is now stored on the heap!
+                                               // this means is it now not STATIC -> it can be increased
+                                               // '::from' is requesting the needed memory
+    println!("The string is {}", s);
+
+    // If we want to modify the String on the heap
+    s.push_str(", world!");
+    println!("The string is {}", s);
+    // IMPORTANT -> The allocated memory is automatically freed when the variable is out of scope!
 }
