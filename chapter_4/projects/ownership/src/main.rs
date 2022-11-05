f29549fe62b52f66290a2316c9f967ef81bb55cf
JMariadlcs <jmariadelacruzsanchez@gmail.com>
2022-11-05 13:58:51 +0100

    Functions and ownership

--- chapter_4/projects/ownership/src/main.rs
+++ chapter_4/projects/ownership/src/main.rs
@@ -34,13 +34,44 @@ fn main() {
     println!("s1 = {}, s2 = {}", s1, s2);
 
     // ----- COPY ------
-    
+
     // In this case x is still valid altough 'clone()' was not used
     // This is because integers are stored on the STACK not on the HEAP
     let x = 5;
     let y = x;
 
     println!("x = {}, y = {}", x, y);
+
+    let tup: (i32, bool) = (7, true);
+    let tup_clone = tup;
+    println!("Tups first parameters are tup:{} and tup_clone:{}", tup.1, tup_clone.1);
     
-    
-}
+    // More types that can be copied (stored on the stack)
+    // u32, bool, f64, char, tuple with only copy types 
+
+    // ----- OWNERSHIP AND FUNCTIONS ------
+
+    let s = String::from("hello");  // s comes into scope
+
+    takes_ownership(s); // s's value moves into the function...
+                                    // ... and so is no longer valid here
+
+    let x = 5;                 // x comes into scope
+
+    makes_copy(x);     // x would move into the function,
+                                    // but i32 is Copy, so it's okay to still
+                                    // use x afterward
+    println!("As x is integer it can be still used here, x is: {}", x)
+}   // Here, x goes out of scope, then s. But because s's value was moved, nothing
+    // special happens.
+
+// ----- OWNERSHIP AND FUNCTIONS ------
+
+fn takes_ownership(some_string: String) { // some_string comes into scope
+    println!("{}", some_string);
+} // Here, some_string goes out of scope and `drop` is called. The backing
+  // memory is freed.
+
+fn makes_copy(some_integer: i32) { // some_integer comes into scope
+    println!("{}", some_integer);
+} // Here, some_integer goes out of scope. Nothing special happens.
