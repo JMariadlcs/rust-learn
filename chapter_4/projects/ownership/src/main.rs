07e9cf0c41d8938b9aa675b03f57fc2f20bbab88
JMariadlcs <jmariadelacruzsanchez@gmail.com>
2022-11-05 13:48:27 +0100

    move and clone

--- chapter_4/projects/ownership/src/main.rs
+++ chapter_4/projects/ownership/src/main.rs
@@ -8,5 +8,28 @@ fn main() {
     // If we want to modify the String on the heap
     s.push_str(", world!");
     println!("The string is {}", s);
-    // IMPORTANT -> The allocated memory is automatically freed when the variable is out of scope!
+    // IMPORTANT -> The allocated memory is AUTOMATICALLY freed when the variable is out of scope!
+
+    // ----- MOVE ------
+
+    // Integers are simple values stored on the STACK so they can be copied 
+    let x = 5;
+    let y = x;
+    println!("The var are {}, {}", x, y); // Both are valid and accesible because are stored on the STACK
+    
+    // String types are stored on the HEAP not on the STACK
+    let s1 = String::from("hello");
+    let s2 = s1; // When s1 is copied on s2 -> s1 is NO LONGER VALID // This is called "MOVE"
+    // println!("{}, world!", s1); -> This results on an error because s1 IS NOT LONGER VALID
+    println!("The var is {}", s2);
+    
+
+    // ----- CLONE ------
+    // By cloning, s1 would be still be accesible after scope
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+
+    println!("s1 = {}, s2 = {}", s1, s2);
+    
+    
 }
