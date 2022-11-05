f62a14d9c554083f0dd076abf932a79d54e998dd
JMariadlcs <jmariadelacruzsanchez@gmail.com>
2022-11-05 12:41:50 +0100

    if expressions

--- chapter_3/projects/control_flow/src/main.rs
+++ chapter_3/projects/control_flow/src/main.rs
@@ -4,6 +4,13 @@ fn main() {
     // If expressions
     let returned: bool = grater_than_number(72);
     println!("Comparison returned {}", returned);
+    // Match function
+    // match_function(5);
+
+    // If in an expression
+    let number = -4;
+    let is_positive = if number > 0 { true } else { false };
+    println!("Value is positive: {}", is_positive)
 }
 
 // If expressions
@@ -14,3 +21,16 @@ fn grater_than_number(x: i32) -> bool {
         false 
     }
 }
+
+// Using Match statement (another way of comparison)
+// Not correct (I am not sure how to implement with 'match' yet.)
+/* 
+fn match_function(number: u32) {
+    match number {
+        number % 4 == 0 => println!("number is divisible by 4");
+        number % 3 == 0 => println!("number is divisible by 3");
+        number % 2 == 0 => println!("number is divisible by 2");
+        _ => println!("number is not divisible by 4, 3, or 2");
+    }
+}
+*/
