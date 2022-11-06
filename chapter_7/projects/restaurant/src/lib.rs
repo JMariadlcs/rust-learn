7eea4b344398ad9aee943648183f76f675e397c9
JMariadlcs <jmariadelacruzsanchez@gmail.com>
2022-11-06 19:46:50 +0100

    reorg

--- chapter_7/projects/restaurant/src/lib.rs
+++ chapter_7/projects/restaurant/src/lib.rs
@@ -1,19 +1,5 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
 
-        pub fn seat_at_table() {}
-    }
-
-    pub mod serving {
-        pub fn take_order() {}
-
-        pub fn serve_order() {}
-
-        pub fn take_payment() {}
-    }
-}
-
+mod front_of_house;
 // Use with abs path and 'pub' so other scopes can use it
 pub use crate::front_of_house::hosting;
 // Use with relative path
