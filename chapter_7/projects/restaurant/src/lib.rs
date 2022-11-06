347b4d468aeeb263cdd387514cb18a9bcbcc6981
JMariadlcs <jmariadelacruzsanchez@gmail.com>
2022-11-06 19:25:02 +0100

    struct impl

--- chapter_7/projects/restaurant/src/lib.rs
+++ chapter_7/projects/restaurant/src/lib.rs
@@ -20,4 +20,41 @@ pub fn eat_at_restaurant() { // Can refer to 'front_of_house' altough it is priv
 
     // Relative path
     front_of_house::hosting::add_to_waitlist();
+
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal
+    // meal.seasonal_fruit = String::from("blueberries");
+}
+
+
+// REF TO RELATIVE PATHS USING 'SUPER'
+fn deliver_order() {}
+
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
 }
