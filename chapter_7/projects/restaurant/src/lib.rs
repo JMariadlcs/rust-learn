8499fc0bb94c6b65f1285674883bcdefb04e6701
JMariadlcs <jmariadelacruzsanchez@gmail.com>
2022-11-06 19:04:20 +0100

    lib.rs and modules

--- chapter_7/projects/restaurant/src/lib.rs
+++ chapter_7/projects/restaurant/src/lib.rs
@@ -1,14 +1,16 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
 #[cfg(test)]
-mod tests {
-    use super::*;
+mod front_of_house {
+    mod hosting {
+        fn add_to_waitlist() {}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
     }
 }
