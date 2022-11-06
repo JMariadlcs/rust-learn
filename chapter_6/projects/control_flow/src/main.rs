b0e6a781bf8d5543ba8eb3000e942347edc90d69
JMariadlcs <jmariadelacruzsanchez@gmail.com>
2022-11-06 18:16:58 +0100

    coin example

--- chapter_6/projects/control_flow/src/main.rs
+++ chapter_6/projects/control_flow/src/main.rs
@@ -1,3 +1,38 @@
+#[derive(Debug)] // so we can inspect the state in a minute
+
+enum UsState {
+    NewYork,
+    California
+}
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, Control Flow!");
+    let penny = Coin::Penny;
+    let result = value_in_cents(penny);
+    println!("The result was {} penny", result);
+
+    let quarter = Coin::Quarter(UsState::California);
+    let result = value_in_cents(quarter);
+    println!("The result was {} Quarter", result);
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}", state);
+            25
+        }
+    }
 }
