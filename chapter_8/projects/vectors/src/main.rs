da7cca94532c5315660b7662a05740dd7538317d
JMariadlcs <jmariadelacruzsanchez@gmail.com>
2022-11-07 18:54:23 +0100

    vectors 0.1

--- chapter_8/projects/vectors/src/main.rs
+++ chapter_8/projects/vectors/src/main.rs
@@ -1,3 +1,32 @@
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, Vectors!");
+
+    // DEF OF VECTOR
+    let v: Vec<i32> = Vec::new();
+
+    // DEF AND INITIALIZATION OF VECTOR -> Rust sets type
+    let v = vec![1, 2, 3]; // all must be same value
+
+    // UPDATE VECTORS
+    let mut vec_updatable = Vec::new(); // must be 'mut'
+    vec_updatable.push(5);
+    println!("Vector is {:?}", vec_updatable);
+
+    let mut v2 = vec![1, 2, 3]; 
+    v2.push(4);
+    println!("Vector is {:?}", v2);
+
+    // READ VECTOR ELEMENTS
+    let v = vec![1, 2, 3, 4, 5];
+
+    let third: &i32 = &v[2];
+    println!("The third element is {}", third);
+
+    let third: Option<&i32> = v.get(2);
+    match third { // '.get' returns an Option<> so 'Some' and 'None' must be implemented
+        Some(third) => println!("The third element is {}", third),
+        None => println!("There is no third element."),
+    }
 }
