defed8fefc9d0bc633e496789f46d749e0774f69
JMariadlcs <jmariadelacruzsanchez@gmail.com>
2022-11-08 18:22:32 +0100

    RUST BACKTRACE

--- chapter_9/projects/error_handling/src/main.rs
+++ chapter_9/projects/error_handling/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, Error Handling!");
+
+
+    // PANIC
+    // To directly exit execution
+    // panic!("crash and burn");
+
+    // panic! BACKTRACE
+    // execute: 'RUST_BACKTRADE=1 cargo run'
+    let v = vec![1, 2, 3];
+    v[99];
 }
