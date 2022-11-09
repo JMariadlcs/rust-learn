9fe83d9b2147f42186a3927e14020548edba1873
JMariadlcs <jmariadelacruzsanchez@gmail.com>
2022-11-09 19:36:24 +0100

    lifetimes

--- chapter_10/projects/lifetimes/src/main.rs
+++ chapter_10/projects/lifetimes/src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    println!("Hello, Lifetimes!");
+    let x = 5;            // ----------+-- 'b
+                          //           |
+    let r = &x;           // --+-- 'a  |
+                          //   |       |
+    println!("r: {}", r); //   |       |
+                          // --+       |
+                         // ----------+
+
+
+    // LIFETIMES IN FUNCTIONS
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str  {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
 }
