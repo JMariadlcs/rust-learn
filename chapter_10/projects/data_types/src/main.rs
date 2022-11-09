e2a1b1f79ef182aae9cac1d762ab02ac66e3f4ff
JMariadlcs <jmariadelacruzsanchez@gmail.com>
2022-11-09 18:12:44 +0100

    structs and enums

--- chapter_10/projects/data_types/src/main.rs
+++ chapter_10/projects/data_types/src/main.rs
@@ -1,3 +1,63 @@
+fn largest_i32(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn largest_char(list: &[char]) -> &char {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+// WE CAN USE GENERIC TYPE
+/* 
+fn largest<T>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+} */
+
 fn main() {
-    println!("Hello, world!");
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest_i32(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest_char(&char_list);
+    println!("The largest char is {}", result);
+
+    
+// WE CAN USE GENERIC TYPE
+/* 
+let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
+    */
 }
