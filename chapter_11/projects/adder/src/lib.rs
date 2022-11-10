b1e23a94652404d9050431421c0fd7f7abeed3fd
JMariadlcs <jmariadelacruzsanchez@gmail.com>
2022-11-10 17:49:29 +0100

    first test

--- chapter_11/projects/adder/src/lib.rs
+++ chapter_11/projects/adder/src/lib.rs
@@ -1,14 +1,10 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+// Run: cargo test
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     #[test]
     fn it_works() {
-        let result = add(2, 2);
+        let result = 2 + 2;
         assert_eq!(result, 4);
     }
 }
